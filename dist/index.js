2f660dc4e38d33296ed0c920729be682d6c7df87
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-10-11 20:48:32 -0700

    create todo interface

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,24 @@
 "use strict";
+const todos = [];
 const btn = document.getElementById("btn");
 const input = document.getElementById("todo-input");
 const form = document.querySelector("form");
 const list = document.getElementById("todolist");
 function handleSubmit(e) {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    input.value = "";
+}
+function createTodo(todo) {
     const newLI = document.createElement("LI");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    newLI.append(newTodoText);
+    newLI.append(todo.text);
     newLI.append(checkbox);
     list === null || list === void 0 ? void 0 : list.append(newLI);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+const todos: Todo[] = [];
+
 const btn = document.getElementById("btn");
 const input = document.getElementById("todo-input")! as HTMLInputElement;
 const form = document.querySelector("form")!;
@@ -5,11 +11,21 @@ const list = document.getElementById("todolist");
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  const newTodoText = input.value;
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
   const newLI = document.createElement("LI");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  newLI.append(newTodoText);
+  newLI.append(todo.text);
   newLI.append(checkbox);
   list?.append(newLI);
 }
