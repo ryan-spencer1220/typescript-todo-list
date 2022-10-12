bfb904d81a5c9b66a9e47d30b2b483c204fba8e0
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-10-11 20:09:23 -0700

    add primary todo list functionality

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("HELLO");
+const btn = document.getElementById("btn");
+const input = document.getElementById("todo-input");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodoText = input.value;
+    const newLI = document.createElement("LI");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLI.append(newTodoText);
+    newLI.append(checkbox);
+    list === null || list === void 0 ? void 0 : list.append(newLI);
+}
+form.addEventListener("submit", handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("HELLO");
+const btn = document.getElementById("btn");
+const input = document.getElementById("todo-input")! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const list = document.getElementById("todolist");
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodoText = input.value;
+  const newLI = document.createElement("LI");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newLI.append(newTodoText);
+  newLI.append(checkbox);
+  list?.append(newLI);
+}
+
+form.addEventListener("submit", handleSubmit);
